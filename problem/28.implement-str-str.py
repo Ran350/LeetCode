b1a7fb5ea85ea93d4643486a16c714d665028e68
Hoshi Ran <ureshinostyle350@gmail.com>
2021-07-05 00:29:42 +0900

    [add] 28. Implement strStr()

---
+++ problem/28.implement-str-str.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=28 lang=python3
+#
+# [28] Implement strStr()
+#
+
+# @lc code=start
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if(needle == ""):
+            return 0
+
+        for i in range(len(haystack)):
+            if(haystack[i:i+len(needle)] == needle):
+                return i
+        return -1
+
+# @lc code=end
